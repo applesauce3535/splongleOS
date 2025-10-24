455798d0d5e171f40753eb3f80eafd0e6f47e070
applesauce3535 <bjohnson03123@gmail.com>
2025-10-24 16:43:06 -0500

    i made it brown

--- src/kernel/stdio.c
+++ src/kernel/stdio.c
@@ -5,7 +5,7 @@
 
 const unsigned SCREEN_WIDTH = 80;
 const unsigned SCREEN_HEIGHT = 25;
-const uint8_t DEFAULT_COLOR = 0x02;
+const uint8_t DEFAULT_COLOR = 0x61;
 
 volatile bool vga_lock = false;
 
